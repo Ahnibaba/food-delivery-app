b8561fb525bbb1bbcce2490e98a8f178209fa846
Ahnibaba <aniebietedet2025@gmail.com>
2024-09-01 18:32:18 +0100

    admin

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     "http://localhost:5173",
     "http://localhost:5174", 
     "https://food-delivery-app-swj3.vercel.app",
-    "https://food-delivery-app-rose-one.vercel.app"
+    "https://food-delivery-app-rose-one.vercel.app",
+    "https://food-delivery-app-sspo.vercel.app"
     
 ]
 
